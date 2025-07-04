9646d30139dc846cfdc5ed4df12bf91f3c7b050a
Jefferson Brasilino <jeffersonbrasilino@MacBook-Air-de-Jefferson.local>
2025-07-04 15:41:01 -0300

    adjust

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ const isDev = process.env.NODE_ENV === 'development'
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: 'jeffersonbrasilino.github.io',
+    baseURL: '/jeffersonbrasilino.github.io/',
   },
   modules: [
     '@vueuse/nuxt',
